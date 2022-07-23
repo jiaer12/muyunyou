b791cd37d5afb0de13eff59fd9f309e96af09d10
jiaer12 <93109494+jiaer12@users.noreply.github.com>
2022-07-23 17:12:29 +0800

    Add files via upload

---
+++ src/pages/index/components/slider/module/defaults.js
@@ -0,0 +1,8 @@
+// 默认参数
+const DEFAULTS ={
+    initialIndex:0,             // 初始索引（第0张照片）
+    animation:true,             // 切换时是否有动画
+    speed:300,                  // 切换速度，单位ms
+    autoplay:0                  // 自动切换，单位ms（0为不开启自动切换，其他正数为开启自动切换）
+};
+export default DEFAULTS;
---
+++ src/pages/index/index.js
@@ -0,0 +1,23 @@
+
+// 引入公共样式
+import 'style/reset.css'
+import 'style/base.css'
+import './css/css.css';
+
+//组件的
+import 'components/topbar';     // 会自动找topbar下的index.js文件
+import './components/slider'
+import './components/jjzyx'
+import 'components/backtotop';
+import './components/menu'
+import 'components/main-nav'
+import 'components/loading'
+
+import './components/xxsw'
+import './components/zsj'
+import './components/tsddty'
+import './components/hot-ads'
+import './components/ddwl'
+import './components/gt'
+
+import 'components/footer'
